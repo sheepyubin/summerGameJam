529df531a069fcd53dc18d5591e2c0110804163d
sheepyubin <yangyubinstory@naver.con>
2023-07-12 00:59:11 +0900

    .
    .

--- 2023_summer_GameJam/Assets/Title/SecenChanger.cs
+++ 2023_summer_GameJam/Assets/Title/SecenChanger.cs
@@ -2,11 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class SecenChanger : MonoBehaviour
 {
-    public void StartToLoading()
+    public Image fadePanel;
+    public float fadeDuration = 1.0f;
+
+    private void Start()
     {
-        SceneManager.LoadScene("loding");
+        fadePanel.color = new Color(fadePanel.color.r, fadePanel.color.g, fadePanel.color.b, 1f);
+        fadePanel.gameObject.SetActive(true);
+    }
+
+    public void FadeOutAndLoadScene(string sceneName)
+    {
+        // ÆäÀÌµå ¾Æ¿ô ÈÄ ¾À ÀüÈ¯
+        FadeOut(() =>
+        {
+            SceneManager.LoadScene("loding");
+        });
+    }
+
+    private void FadeIn()
+    {
+        LeanTween.alpha(fadePanel.rectTransform, 0f, fadeDuration)
+            .setEase(LeanTweenType.linear)
+            .setOnComplete(() => fadePanel.gameObject.SetActive(false));
+    }
+
+    private void FadeOut(System.Action onComplete = null)
+    {
+        fadePanel.gameObject.SetActive(true);
+        LeanTween.alpha(fadePanel.rectTransform, 1f, fadeDuration)
+            .setEase(LeanTweenType.linear)
+            .setOnComplete(() => onComplete?.Invoke());
+    }
+
+    public void SceneChange()
+    {
+        FadeIn();
     }
 
 }
